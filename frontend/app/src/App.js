c063cdd1f10ea78a42c058b7fb038c1aa6b19522
nikol4e2 <arsovnikola1@gmail.com>
2025-04-22 23:02:49 +0200

    changes to APP

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -6,7 +6,8 @@ import Home from "./components/homeComponents/home/Home";
 import Footer from "./components/footer/Footer";
 import AboutUs from "./components/aboutUs/aboutUs";
 import Registration from "./components/registration/Registration";
-
+import Contact from "./components/contact/Contact";
+import Gallery from "./components/gallery/Gallery";
 function App() {
   return (
     <div className="App">
@@ -15,6 +16,8 @@ function App() {
           <Route path="/" element={<Home></Home>}></Route>
           <Route path="/about-us" element={<AboutUs></AboutUs>}></Route>
           <Route path="/registration" element={<Registration></Registration>}></Route>
+          <Route path="/contact" element={<Contact></Contact>}></Route>
+          <Route path="/photos" element={<Gallery></Gallery>}></Route>
       </Routes>
         <Footer/>
     </div>
