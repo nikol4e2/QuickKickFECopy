5b322a30be04633490e7369b0cb346c41962d435
nikol4e2 <arsovnikola1@gmail.com>
2026-08-27 22:10:06 +0200

    added vercel analytics

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -17,7 +17,7 @@ import ListLiveMatch from "./components/LIVE/ListLiveMatch/ListLiveMatch";
 import LiveMatchStatistics from "./components/LIVE/LiveMatchStatistics/LiveMatchStatistics";
 import Login from "./AdminPanel/Auth/Login";
 import React from "react";
-
+import { Analytics } from '@vercel/analytics/react';
 function App() {
   return (
     <div className="App">
@@ -38,7 +38,7 @@ function App() {
 
       </Routes>
         <Footer/>
-
+        <Analytics />
     </div>
   );
 }
